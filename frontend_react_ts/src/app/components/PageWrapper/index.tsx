6583d5fd1b1f95a68686949c6118338e805152d9
Oakley <oakleypeavler@gmail.com>
2022-07-05 14:04:00 -0400

    nav bar consolidation, index component drawer

---
+++ frontend_react_ts/src/app/components/PageWrapper/index.tsx
@@ -0,0 +1,210 @@
+import { AppBar } from 'app/components/AppBar/Loadable';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { useDispatch, useSelector } from 'react-redux';
+import { useThemeSlice } from 'styles/theme/slice';
+import { selectThemeKey } from 'styles/theme/slice/selectors';
+import { DarkMode, LightMode } from '@mui/icons-material';
+
+const pages = [];
+const settings = ['Account', 'Logout'];
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export function PageWrapper({ children }: Props) {
+  const dispatch = useDispatch();
+  const { actions } = useThemeSlice();
+
+  const currentTheme = useSelector(selectThemeKey);
+
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null,
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null,
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const darkModeIcon =
+    currentTheme === 'dark' ? (
+      <LightMode onClick={() => dispatch(actions.changeTheme('light'))} />
+    ) : (
+      <DarkMode onClick={() => dispatch(actions.changeTheme('dark'))} />
+    );
+
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontWeight: 700,
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Freebets
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map(page => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Freebets
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map(page => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Stack direction="row" spacing={1}>
+                <Tooltip title="Toggle dark mode">
+                  <IconButton color="inherit">
+                    <Badge color="secondary">{darkModeIcon}</Badge>
+                  </IconButton>
+                </Tooltip>
+                {/* <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip> */}
+              </Stack>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map(setting => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: theme =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+        }}
+      >
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          {children}
+        </Container>
+      </Box>
+    </>
+  );
+}
--- frontend_react_ts/src/app/pages/BetViewPage/index.tsx
+++ frontend_react_ts/src/app/pages/BetViewPage/index.tsx
@@ -13,76 +13,20 @@ import { Copyright } from 'app/components/Copyright';
 import MenuIcon from '@mui/icons-material/Menu';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { BetView } from './Features/BetView';
+import { PageWrapper } from 'app/components/PageWrapper';
 
 export function BetViewPage({ match }) {
   const { params } = match;
 
   return (
-    <>
-      <Helmet>
-        <title>Freebets - View Play</title>
-        <meta name="description" content="Freebets - View Play" />
-      </Helmet>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" open={false}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() => {}}
-              sx={{
-                marginRight: '36px',
-                // ...(false && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              Freebets - View Play
-            </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-          <Box
-            component="main"
-            sx={{
-              backgroundColor: theme =>
-                theme.palette.mode === 'light'
-                  ? theme.palette.grey[100]
-                  : theme.palette.grey[900],
-              flexGrow: 1,
-              height: '100vh',
-              overflow: 'auto',
-            }}
-          >
-            <Toolbar />
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-              <Grid container spacing={3}>
-                {/* Recent Orders */}
-                <Grid item xs={12}>
-                  <BetView betId={params.betId} />
-                </Grid>
-              </Grid>
-              <Copyright sx={{ pt: 4 }} />
-            </Container>
-          </Box>
-        </AppBar>
-      </Box>
-    </>
+    <PageWrapper>
+      <Grid container spacing={3}>
+        {/* Bet View */}
+        <Grid item xs={12}>
+          <BetView betId={params.betId} />
+        </Grid>
+      </Grid>
+      <Copyright sx={{ pt: 4 }} />
+    </PageWrapper>
   );
 }
