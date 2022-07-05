90af09b7515242ac193549ae3b029deb9089a038
Oakley <oakleypeavler@gmail.com>
2022-07-05 14:04:00 -0400

    nav bar consolidation, index component drawer

--- frontend_react_ts/src/app/components/NotFoundPage/index.tsx
+++ frontend_react_ts/src/app/components/NotFoundPage/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import styled from 'styled-components/macro';
 import { P } from './P';
 import { Helmet } from 'react-helmet-async';
+import { PageWrapper } from '../PageWrapper';
 
 export function NotFoundPage() {
   return (
-    <>
+    <PageWrapper>
       <Helmet>
         <title>404 Page Not Found</title>
         <meta name="description" content="Page not found" />
@@ -20,7 +21,7 @@ export function NotFoundPage() {
         </Title>
         <P>Page not found.</P>
       </Wrapper>
-    </>
+    </PageWrapper>
   );
 }
 
--- frontend_react_ts/src/app/pages/HomePage/index.tsx
+++ frontend_react_ts/src/app/pages/HomePage/index.tsx
@@ -1,99 +1,28 @@
-import { AppBar } from 'app/components/AppBar/Loadable';
 import { BetIndex } from './Features/BetsIndex';
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import { Copyright } from 'app/components/Copyright';
-import MenuIcon from '@mui/icons-material/Menu';
-import { useDispatch, useSelector } from 'react-redux';
-import { useThemeSlice } from 'styles/theme/slice';
-import { selectThemeKey } from 'styles/theme/slice/selectors';
-import { DarkMode, LightMode } from '@mui/icons-material';
+import { PageWrapper } from 'app/components/PageWrapper';
+import headerImage from './assets/wow.gif';
 
 export function HomePage() {
-  const dispatch = useDispatch();
-  const { actions } = useThemeSlice();
-
-  const currentTheme = useSelector(selectThemeKey);
-
-  const darkModeIcon =
-    currentTheme === 'dark' ? (
-      <LightMode onClick={() => dispatch(actions.changeTheme('light'))} />
-    ) : (
-      <DarkMode onClick={() => dispatch(actions.changeTheme('dark'))} />
-    );
-
   return (
     <>
       <Helmet>
-        <title>Freebets - Plays</title>
+        <title>Plays</title>
         <meta name="description" content="Freebets - Daily Plays" />
       </Helmet>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" open={false}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={() => {}}
-              sx={{
-                marginRight: '36px',
-                // ...(false && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              Freebets - Home
-            </Typography>
-            <IconButton color="inherit">
-              <Badge color="secondary">{darkModeIcon}</Badge>
-            </IconButton>
-          </Toolbar>
-          <Box
-            component="main"
-            sx={{
-              backgroundColor: theme =>
-                theme.palette.mode === 'light'
-                  ? theme.palette.grey[100]
-                  : theme.palette.grey[900],
-              flexGrow: 1,
-              height: '100vh',
-              overflow: 'auto',
-            }}
-          >
-            <Toolbar />
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-              <Grid container spacing={3}>
-                {/* Recent Orders */}
-                <Grid item xs={12}>
-                  <BetIndex />
-                </Grid>
-              </Grid>
-              <Copyright sx={{ pt: 4 }} />
-            </Container>
-          </Box>
-        </AppBar>
-      </Box>
+      <PageWrapper>
+        <Grid container spacing={3}>
+          {/* Bet Index */}
+          <Grid item xs={12}>
+            {/* <img src={headerImage} alt="" style={{ width: '100%' }} /> */}
+            <BetIndex />
+          </Grid>
+        </Grid>
+        <Copyright sx={{ pt: 4 }} />
+      </PageWrapper>
     </>
   );
 }
