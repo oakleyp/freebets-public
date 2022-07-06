d3162a71f8435e258b8330f9b3941553478b91fd
Oakley <oakleypeavler@gmail.com>
2022-07-06 16:19:47 -0400

    delete frontend_react

--- frontend_react/app/containers/ViewBetPage/constants.js
+++
@@ -1,5 +0,0 @@
-
-export const SET_BET_ID = 'freebets/ViewBetPage/SET_BET_ID';
-export const LOAD_BET = 'freebets/ViewBetPage/LOAD_BET';
-export const LOAD_BET_SUCCESS = 'freebets/ViewBetPage/LOAD_BET_SUCCESS';
-export const LOAD_BET_ERROR = 'freebets/ViewBetPage/LOAD_BET_ERROR';
