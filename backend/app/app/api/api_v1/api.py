7996993a36ce8fe101c7263014ed731923a323ab
Oakley <oakleypeavler@gmail.com>
2022-07-07 10:21:12 -0400

    add route variants to prevent 307

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -1,29 +1,7 @@
 from typing import Callable, Any
-from fastapi import APIRouter as FastAPIRouter
+from fastapi import APIRouter
 
 from app.api.api_v1.endpoints import bets, items, login, users, utils
-
-class APIRouter(FastAPIRouter):
-    def api_route(
-        self, path: str, *, include_in_schema: bool = True, **kwargs: Any
-    ) -> Callable:
-        if path.endswith("/"):
-            path = path[:-1]
-
-        add_path = super().api_route(
-            path, include_in_schema=include_in_schema, **kwargs
-        )
-
-        alternate_path = path + "/"
-        add_alternate_path = super().api_route(
-            alternate_path, include_in_schema=False, **kwargs
-        )
-
-        def decorator(func: Callable) -> Callable:
-            add_alternate_path(func)
-            return add_path(func)
-
-        return decorator
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
